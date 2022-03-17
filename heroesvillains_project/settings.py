37b617d64bcea34f1f373c6a11dfe829bcdae267
Amanda <aclaas522@gmail.com>
2022-03-17 14:57:35 -0500

    Set Up Database

--- heroesvillains_project/settings.py
+++ heroesvillains_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--s&&#@n+k6+_^c6^vo33$wf6wvn9pka!wv%ebx57ty4d_(l(z&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroesvillains_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +111,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroesvillains_project.local_settings import *
+except ImportError:
+    pass
