5020cbbeb870e3b82bbde36e874c8920a0efd64c
Amanda <aclaas522@gmail.com>
2022-03-18 15:09:19 -0500

    Updated Project URLs + Created First View Function + Created Supers App urls File + Updated Request in Views File

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view([''])
+@api_view(['GET'])
 def supers_list(request):
 
     return Response('ok')
-    
+
