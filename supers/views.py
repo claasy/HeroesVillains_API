f357ba646a74082d7776c8f02cecbf18ef8e623e
Amanda <aclaas522@gmail.com>
2022-03-18 15:58:28 -0500

    Updated Views File in Supers App

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
 
 @api_view(['GET'])
 def supers_list(request):
+    supers = Super.objects.all()
 
-    return Response('ok')
+    serializer = SuperSerializer(supers, many=True)
 
+
+    return Response(serializer.data)
